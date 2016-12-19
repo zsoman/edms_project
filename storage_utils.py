aab8595b5a076a58d8255e4920a497315610fcc4
Zsolt Bokor Levente <bokor.zsolt5@gmail.com>
2016-12-19 14:30:10 +0100

    Added docstrings to storage_utils.py file.

--- storage_utils.py
+++ storage_utils.py
@@ -1,10 +1,28 @@
-"""Various utils for making file-based data management easier."""
+#!/usr/bin/env python
+"""Various utils for making file-based data management easier.
+"""
 
+# Imports -----------------------------------------------------------------------------------------------------------
 import os
 
+# Authorship information  -------------------------------------------------------------------------------------------
+__author__ = "Imre Piller"
+__copyright__ = "Copyright 2016, Morgan Stanley - Training 360 Project"
+__credits__ = __author__
+__version__ = "1.0.0"
+__maintainer__ = "Zsolt Bokor Levente"
+__email__ = ["bokor.zsolt5@gmail.com", "bokorzsolt@yahoo.com"]
+__status__ = "Development"
+
+
+# -------------------------------------------------------------------------------------------------------------------
 
 def get_next_id(storage_path):
-    """Calculate the next available identifier."""
+    """Calculate the next available identifier.
+
+    :param storage_path: The path where to search for the next ID.
+    :return: Integer, next ID.
+    """
     existing_ids = os.listdir(storage_path)
     integer_ids = []
     for i in existing_ids:
