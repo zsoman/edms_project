a5cdb985f569540d07488e25124c2acf59cafd68
Zsolt Bokor Levente <bokor.zsolt5@gmail.com>
2016-12-19 20:08:35 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'edms_project',
+    version = '1.0.0',
+    packages = ['docgen', 'usergen', 'iniformat', 'messenger', 'web_application'],
+    url = 'https://github.com/zsoman/edms_project',
+    license = '',
+    author = 'Zsolt Bokor Levente',
+    author_email = 'bokor.zsolt5@gmail.com',
+    description = 'Electronic Document Management System'
+)
